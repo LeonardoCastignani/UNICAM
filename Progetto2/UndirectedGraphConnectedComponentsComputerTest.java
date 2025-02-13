61c07c4e4aea300cc2750f9aac882cd8352d87d0
Leonardo Castignani <166705836+LeonardoCastignani@users.noreply.github.com>
2025-02-13 18:09:47 +0100

    Update UndirectedGraphConnectedComponentsComputerTest.java

--- Progetto2/UndirectedGraphConnectedComponentsComputerTest.java
+++ Progetto2/UndirectedGraphConnectedComponentsComputerTest.java
@@ -1,5 +1,3 @@
-package it.unicam.cs.asdl2425.mp2;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.HashSet;
@@ -9,17 +7,13 @@ import org.junit.jupiter.api.Test;
 
 /**
  * Classe di test per la classe UndirectedGraphConnectedComponentsComputer.
- * 
- * @author Luca Tesei
- *
  */
 class UndirectedGraphConnectedComponentsComputerTest {
 
     @Test
     final void testComputeConnectedComponentsExceptionsAndTrivialCases() {
         UndirectedGraphConnectedComponentsComputer<String> c = new UndirectedGraphConnectedComponentsComputer<String>();
-        assertThrows(NullPointerException.class,
-                () -> c.computeConnectedComponents(null));
+        assertThrows(NullPointerException.class, () -> c.computeConnectedComponents(null));
         Graph<String> g = new AdjacencyMatrixUndirectedGraph<String>();
         Set<Set<GraphNode<String>>> result = new HashSet<>();
         assertTrue(c.computeConnectedComponents(g).equals(result));
@@ -33,8 +27,7 @@ class UndirectedGraphConnectedComponentsComputerTest {
         comp2.add(new GraphNode<String>("b"));
         result.add(comp2);
         assertTrue(c.computeConnectedComponents(g).equals(result));
-        g.addEdge(new GraphEdge<String>(new GraphNode<String>("a"),
-                new GraphNode<String>("b"), false));
+        g.addEdge(new GraphEdge<String>(new GraphNode<String>("a"), new GraphNode<String>("b"), false));
         result.remove(comp2);
         comp1.add(new GraphNode<String>("b"));
         assertTrue(c.computeConnectedComponents(g).equals(result));
@@ -48,14 +41,10 @@ class UndirectedGraphConnectedComponentsComputerTest {
         g.addNode(new GraphNode<String>("b"));
         g.addNode(new GraphNode<String>("c"));
         g.addNode(new GraphNode<String>("d"));
-        g.addEdge(new GraphEdge<String>(new GraphNode<String>("a"),
-                new GraphNode<String>("b"), false));
-        g.addEdge(new GraphEdge<String>(new GraphNode<String>("d"),
-                new GraphNode<String>("b"), false));
-        g.addEdge(new GraphEdge<String>(new GraphNode<String>("c"),
-                new GraphNode<String>("b"), false));
-        g.addEdge(new GraphEdge<String>(new GraphNode<String>("c"),
-                new GraphNode<String>("d"), false));
+        g.addEdge(new GraphEdge<String>(new GraphNode<String>("a"), new GraphNode<String>("b"), false));
+        g.addEdge(new GraphEdge<String>(new GraphNode<String>("d"), new GraphNode<String>("b"), false));
+        g.addEdge(new GraphEdge<String>(new GraphNode<String>("c"), new GraphNode<String>("b"), false));
+        g.addEdge(new GraphEdge<String>(new GraphNode<String>("c"), new GraphNode<String>("d"), false));
         Set<Set<GraphNode<String>>> result = new HashSet<>();
         Set<GraphNode<String>> comp1 = new HashSet<>();
         comp1.add(new GraphNode<String>("a"));
@@ -67,8 +56,7 @@ class UndirectedGraphConnectedComponentsComputerTest {
 
         g.addNode(new GraphNode<String>("e"));
         g.addNode(new GraphNode<String>("f"));
-        g.addEdge(new GraphEdge<String>(new GraphNode<String>("f"),
-                new GraphNode<String>("e"), false));
+        g.addEdge(new GraphEdge<String>(new GraphNode<String>("f"), new GraphNode<String>("e"), false));
         Set<GraphNode<String>> comp2 = new HashSet<>();
         comp2.add(new GraphNode<String>("e"));
         comp2.add(new GraphNode<String>("f"));
@@ -81,16 +69,13 @@ class UndirectedGraphConnectedComponentsComputerTest {
         result.add(comp3);
         assertTrue(c.computeConnectedComponents(g).equals(result));
 
-        g.addEdge(new GraphEdge<String>(new GraphNode<String>("g"),
-                new GraphNode<String>("c"), false));
+        g.addEdge(new GraphEdge<String>(new GraphNode<String>("g"), new GraphNode<String>("c"), false));
         result.remove(comp3);
         comp1.add(new GraphNode<String>("g"));
         assertTrue(c.computeConnectedComponents(g).equals(result));
 
-        g.addEdge(new GraphEdge<String>(new GraphNode<String>("g"),
-                new GraphNode<String>("g"), false));
-        g.addEdge(new GraphEdge<String>(new GraphNode<String>("g"),
-                new GraphNode<String>("f"), false));
+        g.addEdge(new GraphEdge<String>(new GraphNode<String>("g"), new GraphNode<String>("g"), false));
+        g.addEdge(new GraphEdge<String>(new GraphNode<String>("g"), new GraphNode<String>("f"), false));
         result.remove(comp2);
         comp1.add(new GraphNode<String>("f"));
         comp1.add(new GraphNode<String>("e"));
@@ -98,13 +83,12 @@ class UndirectedGraphConnectedComponentsComputerTest {
 
         Graph<String> g1 = new AdjacencyMatrixUndirectedGraph<String>();
         result.clear();
-        ;
+        
         g1.addNode(new GraphNode<String>("a"));
         g1.addNode(new GraphNode<String>("b"));
-        g1.addEdge(new GraphEdge<String>(new GraphNode<String>("a"),
-                new GraphNode<String>("b"), false));
+        g1.addEdge(new GraphEdge<String>(new GraphNode<String>("a"), new GraphNode<String>("b"), false));
         comp1.clear();
-        ;
+        
         comp1.add(new GraphNode<String>("a"));
         comp1.add(new GraphNode<String>("b"));
         result.add(comp1);
@@ -171,7 +155,6 @@ class UndirectedGraphConnectedComponentsComputerTest {
         result.add(comp);
 
         // Verifica che tutti i nodi siano identificati nella stessa componente
-        assertTrue(c.computeConnectedComponents(g).equals(result),
-                "Un grafo completamente connesso dovrebbe essere una singola componente.");
+        assertTrue(c.computeConnectedComponents(g).equals(result), "Un grafo completamente connesso dovrebbe essere una singola componente.");
     }
 }
